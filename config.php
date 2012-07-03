f219edcb18f52bb80feb58c6d6e197704ab7e059
Wlfzndrhr <wlfzndrhr@gmail.com>
2012-07-03 17:37:49 +0300

    #Commit

--- config.php
+++ config.php
@@ -5,13 +5,13 @@
  * @name $fbconfig
  * @author Mahmud Ahsan
  */
-$fbconfig['appid' ] = "";
-$fbconfig['api']    = "";
-$fbconfig['secret'] = "";
+$fbconfig['appid' ] = "373351179378257";
+$fbconfig['api']    = "stephanekalmesfoto";
+$fbconfig['secret'] = "a767c958cdbf53fcb8451a9e42a7c6b9";
 
-$fbconfig['baseUrl']    =   ""; //"http://thinkdiff.net/demo/newfbconnect1/iframe/isimple";
-$fbconfig['appBaseUrl'] =   ""; //"http://apps.facebook.com/thinkdiffdemotwo/";
-$fbconfig['pageUrl']    =   ""; //"http://www.facebook.com/thinkdiff.net";
+$fbconfig['baseUrl']    =   "https://afternoon-snow-2535.herokuapp.com/";
+$fbconfig['appBaseUrl'] =   "https://apps.facebook.com/stephanekalmesfoto/";
+$fbconfig['pageUrl']    =   "https://www.facebook.com/StephaneKalmesfotografie";
 $fbconfig['appPageUrl'] =   "{$fbconfig['pageUrl']}?sk=app_{$fbconfig['appid' ]}";
 
 $tutorialLink           =   "<a href='http://thinkdiff.net/facebook/isimple-graph-api-iframe-base-facebook-page-application-development' target='_blank'>Tutorial: iSimple - Graph API & Iframe base Facebook Page App Development</a>";
